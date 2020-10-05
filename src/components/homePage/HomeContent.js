88b5d640b0c52721807094370f9fb6cc8314f50b
kalyyan <Kalyyan.2612@gmail.com>
2020-10-05 12:32:05 +0530

    changed the contact page

--- src/components/homePage/HomeContent.js
+++ src/components/homePage/HomeContent.js
@@ -1,25 +1,25 @@
-import React, { useEffect } from "react"
+import React, { useEffect } from 'react';
 
 //Scroll Behaviour
-import { useInView } from "react-intersection-observer"
-import { useAnimation } from "framer-motion"
+import { useInView } from 'react-intersection-observer';
+import { useAnimation } from 'framer-motion';
 
 //Styled Components
-import { Container, SectionHeading } from "../../styles/globalStyles"
-import { HomeContentSection, Content } from "../../styles/homeStyles"
+import { Container, SectionHeading } from '../../styles/globalStyles';
+import { HomeContentSection, Content } from '../../styles/homeStyles';
 
 const HomeContent = () => {
-  const animation = useAnimation()
+  const animation = useAnimation();
   const [contentRef, inView] = useInView({
     triggerOnce: true,
-    rootMargin: "-100px",
-  })
+    rootMargin: '-100px',
+  });
 
   useEffect(() => {
     if (inView) {
-      animation.start("visible")
+      animation.start('visible');
     }
-  }, [animation, inView])
+  }, [animation, inView]);
 
   return (
     <HomeContentSection
@@ -42,9 +42,11 @@ const HomeContent = () => {
         <Content>
           <SectionHeading id="about">01: About Me</SectionHeading>
           <p>
-            I enjoy creating things that live on the internet, whether that be
-            websites, applications, or anything in between. My goal is to always
-            build products that provide pixel-perfect, performant experiences.
+            Hello! I'm Kalyyan,a full-stack developer specializing in building
+            (and occasionally designing) things that live on the internet,
+            whether that be websites, applications, or anything in between. My
+            goal is to always build products that provide pixel-perfect,
+            performant experiences.
             <br /> <br /> I love learning new and better ways to create seamless
             user experiences with clean, efficient, and scalable code. I
             consider work an ongoing education, and I'm always looking for
@@ -56,7 +58,7 @@ const HomeContent = () => {
         </Content>
       </Container>
     </HomeContentSection>
-  )
-}
+  );
+};
 
-export default HomeContent
+export default HomeContent;
